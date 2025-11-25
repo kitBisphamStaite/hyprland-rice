8722eecdb9cd79419d037371a2ccf41da1f577a0
kit <kitbispham10@gmail.com>
2025-11-25 13:27:44 +0000

    Added hypridle, moved the .sh files from scrips to ~/.local/bin, changed move_dot_fiels.sh to now delete the directoried in this git project first before copying as to just get the changes (need to see if there is a better way round this)

--- move_dot_files.sh
+++ move_dot_files.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 current_dir=$(pwd)
 
+echo "Deleting Hyprland config..." 
+rm -r $(pwd)/config
 echo "Copying Hyprland config..." 
 cp -r ~/.config/hypr $(pwd)
 
+echo "Deleting Kitty config..." 
+rm -r $(pwd)/kitty
 echo "Copying Kitty config..." 
 cp -r ~/.config/kitty $(pwd)
 
+echo "Deleting WayBar config..." 
+rm -r $(pwd)/waybar
 echo "Copying WayBar config..." 
 cp -r ~/.config/waybar $(pwd)
 
+echo "Deleting Rofi config..." 
+rm -r $(pwd)/rofi
 echo "Copying Rofi config..." 
 cp -r ~/.config/rofi $(pwd)
+
+echo "Deleting ~/.local/bin ..."
+rm -r $(pwd)/bin
+echo "Copying ~/.local/bin ..." 
+cp -r ~/.local/bin $(pwd)
